b434dc16f5b4f62e6df811ea8382f4e06bd6f344
xbd922 <jpthean@gmail.com>
2024-08-17 21:15:57 +0800

    login css

--- js/script.js
+++ js/script.js
@@ -1,32 +1,13 @@
-// const signUpButton=document.getElementById('signUpButton');
-// const signInButton=document.getElementById('signInButton');
-// const signInForm=document.getElementById('signIn');
-// const signUpForm=document.getElementById('signup');
-
-// signUpButton.addEventListener('click',function(){
-//     signInForm.style.display="none";
-//     signUpForm.style.display="block";
-// })
-// signInButton.addEventListener('click', function(){
-//     signInForm.style.display="block";
-//     signUpForm.style.display="none";
-// })
-
-// function closePage() 
-// {
-//     window.location.href = "index.html";
-// }
-
 const signUpButton = document.getElementById("signUpButton");
 const signInButton = document.getElementById("signInButton");
 const container = document.querySelector(".container");
-const signInForm = document.getElementById("signIn");
-const signUpForm = document.getElementById("signup");
+// const signInForm = document.getElementById("signIn");
+// const signUpForm = document.getElementById("signup");
 
 // Switch to Sign Up Mode
 signUpButton.addEventListener("click", function () {
     container.classList.add("sign-up-mode");
-    signInForm.style.display = "none";
+    // signInForm.style.display = "none";
     // signUpForm.style.display = "block";
 });
 
@@ -34,10 +15,10 @@ signUpButton.addEventListener("click", function () {
 signInButton.addEventListener("click", function () {
     container.classList.remove("sign-up-mode");
     // signInForm.style.display = "block";
-    signUpForm.style.display = "none";
+    // signUpForm.style.display = "none";
 });
 
 // Return to Homepage
-function closePage() {
+function backToHome() {
     window.location.href = "index.html";
 }
