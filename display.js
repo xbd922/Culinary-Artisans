f7cf65986cda10626b81d450ce62562f0b14685f
Jqinnn627 <jiaqin627@gmail.com>
2024-08-18 00:39:14 +0800

    Delete display.js

--- display.js
+++
@@ -1,6 +0,0 @@
-/*Create effect on homepage title image */
-window.addEventListener('load', function() {
-    const target = document.getElementById('target-place');
-    target.classList.add('active');
-  });
-  
